e36d18276fcc62f75464e2d77c20378029525aed
krzysiekwie@gmail.com <krzysiekwie@gmail.com>
2020-02-22 18:55:34 +0100

    Updates

---
+++ precache-manifest.eaf9718e31c0f1729f8bb0ebb8893b89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fef5b78be9c2f5a30f18f8e7e5f7a7",
+    "url": "/wine-pairs/index.html"
+  },
+  {
+    "revision": "eb41d2919445492adbd0",
+    "url": "/wine-pairs/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f79fa016c49426bbe2c1",
+    "url": "/wine-pairs/static/css/main.aea1d542.chunk.css"
+  },
+  {
+    "revision": "eb41d2919445492adbd0",
+    "url": "/wine-pairs/static/js/2.cb15aec8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wine-pairs/static/js/2.cb15aec8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f79fa016c49426bbe2c1",
+    "url": "/wine-pairs/static/js/main.d24a6def.chunk.js"
+  },
+  {
+    "revision": "85b7ced5dccdd43c1988",
+    "url": "/wine-pairs/static/js/runtime-main.111acdde.js"
+  },
+  {
+    "revision": "35a0f067e0258b608a4a7a420821ed1d",
+    "url": "/wine-pairs/static/media/PartybyTom.35a0f067.woff"
+  }
+]);
